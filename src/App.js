b961064223d887ee86f9d96f76aed2beabccb192
agni <yourstrulyagni@gmail.com>
2021-02-06 02:00:17 +0530

    created 2 components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
+import Board from './components/Board';
 
 const App = () => {
   return(
-  <>
-    <h1>Welcome to React Parcel Micro App!</h1>
-    <p>Hard to get more minimal than this React app.</p>
-    <h3>i am a developer</h3>
-  </>
+  <div>
+    <h1>TIC TAC TOE</h1>
+    <Board/>
+  </div>
 );
   };
 export default App;
